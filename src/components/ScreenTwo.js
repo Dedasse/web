d51e4cca2536acfb03ee60058ada63a8783607a4
mikaely <mikael.ylivaara@metropolia.fi>
2022-04-19 20:34:24 +0300

    fix video

--- src/components/ScreenTwo.js
+++ src/components/ScreenTwo.js
@@ -8,42 +8,40 @@ class ScreenTwo extends Component {
     player = {}
     state = {
         video: {
-            src: "/Files/1649612787620-vid1.mp4",
+            src: null,
             isFullscreen: true,
         }
     }
-    componentDidMount() {
+   
+    async loadPDF() {
         const serverUrl = process.env.REACT_APP_SERVER_URL;
-        // load pdf filename
-        axios.get(serverUrl + 'api/loadvideos').then(res => {
-            console.log("res " + res.data)
-            this.setState({fileList: res.data})
-            console.log('/Files/' + this.state.fileList[a].filename)
-            this.player.src({
-                src: /Files/+this.state.fileList[a].filename
-        })
-              this.setState({
-                  video: {
-                      src: /Files/+this.state.fileList[a].filename, //this.state.fileList[a].filename
-                      isFullscreen: true,
-                    }
-              })
-            console.log("asd",this.state.video.src)
+        // load video filename
+        const res= await axios.get(serverUrl + 'api/loadvideos')
+        // set filelist of all videos
+        await this.setState({fileList: res.data})
+        // set first video in filelist to play
+        await this.player.src({
+            src: /Files/+this.state.fileList[a].filename
         })
     }
 
     onPlayerReady(player) {
+        this.loadPDF()
         this.player = player;
     }
 
     onVideoEnd() {
         console.log("Video ended");
+        // next video if there is one
         a++
         this.player.src({
                 src: /Files/+this.state.fileList[a>this.state.fileList.length-1 ? a=0:a].filename
         })
 
         this.player.play()
+    }
+    componentWillUnmount() {
+        if(this.player){this.player.dispose}
     }
 
     render() {
@@ -55,7 +53,7 @@ class ScreenTwo extends Component {
                         width={size.width}
                         controls={false}
                         autoplay={true}
-                        src={this.state.video.src}
+                       // src={this.state.video.src}
                         poster={this.state.video.poster}
                         onEnd={this.onVideoEnd.bind(this)}
                         onReady={this.onPlayerReady.bind(this)}
