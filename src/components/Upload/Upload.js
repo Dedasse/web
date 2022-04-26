67faa748f454380269355010d2645b7edeb5fb7a
elenalaa <elena.laakso@metropolia.fi>
2022-04-26 10:17:22 +0300

    QR code genetator added to admin panel

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -5,10 +5,11 @@ import Publish from './Publish';
 import Inserver from './Inserver';
 import DaDarea from './DaDarea';
 import PlusButton from "./CircleMenu";
+import QRCode from 'react-qr-code';
 
 
 const Upload = () => {
-    
+    const [inputValue, setInputValue] = React.useState('')
 
     return (
         <div className="main-container">
@@ -35,7 +36,19 @@ const Upload = () => {
                 </div>
                 
             </div>
-           
+           <div className="container mx-auto w-2/4" style={{display: "flex", justifyContent:"center", flexDirection:"column", }}>
+               <h1 className="py-4">QR-code</h1>
+               <input style={{marginBottom:"20px"}}
+               onChange={e => setInputValue(e.target.value)}
+               type="text" 
+               placeholder="Enter link or text for QR-Code generation"
+               className="border border-gray-388 w-full h-8 p-4 font-light text-sm focus: outline-none"/>
+
+               <QRCode
+                className="mx-auto mt-20"
+                value={inputValue}
+               />
+           </div>
         </div>
 
     )
